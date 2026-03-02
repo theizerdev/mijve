0f5534eb8acdfa6c03129e80a1ae18007c05ab20
Theizer Gonzalez <theizerdev@gmail.com>
2026-03-02 07:51:34 -0400

    cambios en el wizard

--- resources/views/components/layouts/guest-wizard.blade.php
+++ resources/views/components/layouts/guest-wizard.blade.php
@@ -11,7 +11,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>{{ $title }} — {{ config('app.name') }}</title>
+    <title>{{ $title }} — {{ 'mijve' }}</title>
     <meta name="description" content="{{ config('app.name') }} - Registro de Participante">
 
     <!-- Favicon -->
