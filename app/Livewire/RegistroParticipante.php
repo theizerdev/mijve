5d953825ddc26f602dc48c2d3a9e5a2fc8121642
Theizer Gonzalez <theizerdev@gmail.com>
2026-03-19 08:56:39 -0400

    Permitir que mayores a 40 años se puedan registrar

--- app/Livewire/RegistroParticipante.php
+++ app/Livewire/RegistroParticipante.php
@@ -142,10 +142,14 @@ class RegistroParticipante extends Component
     {
         if ($this->actividad_id && $this->edad !== '') {
             $actividad = Actividad::find($this->actividad_id);
-            if ($actividad && ($this->edad < $actividad->edad_desde || $this->edad > $actividad->edad_hasta)) {
-                $this->addError('edad', "La edad debe estar entre {$actividad->edad_desde} y {$actividad->edad_hasta} años para esta actividad.");
-            } else {
-                $this->resetErrorBag('edad');
+            if ($actividad) {
+                if ($this->edad > 40) {
+                    $this->resetErrorBag('edad');
+                } elseif ($this->edad < $actividad->edad_desde || $this->edad > $actividad->edad_hasta) {
+                    $this->addError('edad', "La edad debe estar entre {$actividad->edad_desde} y {$actividad->edad_hasta} años para esta actividad.");
+                } else {
+                    $this->resetErrorBag('edad');
+                }
             }
         }
     }
